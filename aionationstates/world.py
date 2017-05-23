e3028e26e9ee4771dba85984774665ed3e3d5758
micha <micha030201@gmail.com>
2017-05-23 12:02:35 +0300

    world dispatch fix

--- aionationstates/world.py
+++ aionationstates/world.py
@@ -1,13 +1,18 @@
 from aionationstates.shards import (
-    CensusShard, Dispatchlist, StandardShardCases)
+    CensusShard, Dispatch, Dispatchlist, StandardShardCases)
 
 
 class World(Dispatchlist, CensusShard, StandardShardCases):
 
-    # TODO: dispatchlist parameters, regionsbytag parameters
+    # TODO: regionsbytag parameters
 
     STR_CASES = {'featuredregion'}
     INT_CASES = {'numnations'}
     LIST_CASES = {'newnations', 'regions', 'regionsbytag'}
+
+    async def dispatch(self, id):
+        d = Dispatch(id=id)
+        await d.update()
+        return d
 
 
