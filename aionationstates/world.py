1f9da2a61d4d325f448c0a4777f7b4d9f60c35fa
micha <micha030201@gmail.com>
2017-05-30 21:21:13 +0300

    regionsbytag

--- aionationstates/world.py
+++ aionationstates/world.py
@@ -4,8 +4,18 @@ from aionationstates.shards import Census
 from aionationstates.ns_to_human import dispatch_categories
 
 class World(Census, Session):
-
-    # TODO: regionsbytag parameters
+    def regionsbytag(self, *tags):
+        if len(tags) > 10:
+            raise ValueError('You can specify up to 10 tags')
+        # We don't check for invalid tags here because the behaviour is
+        # fairly intuitive - quering for a non-existent tag returns no
+        # regions, excluding it returns all of them.
+        params = {'tags': ','.join(tags)}
+        def result(root):
+            text = root.find('REGIONS').text
+            return text.split(',') if text else ()
+        return self._compose_api_request(
+            q='regionsbytag', params=params, result=result)
 
     def dispatch(self, id):
         return self._compose_api_request(
@@ -18,6 +28,9 @@ class World(Census, Session):
         params = {'sort': sort}
         if author:
             params['dispatchauthor'] = author
+        # Here we do need to ensure that our categories are valid, cause
+        # NS just ignores the categories it doesn't recognise and returns
+        # whatever it feels like.
         if category and subcategory:
             if (category not in dispatch_categories or
                     subcategory not in dispatch_categories[category]):
